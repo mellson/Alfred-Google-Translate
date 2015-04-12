77bb7c48ef981da40e2b5324293b41b591a1503a
Anders Bech Mellson <mellson@icloud.com>
2015-04-12 13:05:13 +0200

    Updated to the new Google API
    and removed Alfred 1.x versions

--- translate.sh
+++ translate.sh
@@ -12,4 +12,4 @@ set +o histexpand
 TEXT_TO_TRANSLATE=$(echo "$QUERY"|sed 's/ /+/g'|sed 's/'$SOURCE_LANGUAGE'//'|sed 's/'$DESTINATION_LANGUAGE'//')
 
 # Call Google and ask for the answer
-curl -s -A "Mozilla" "http://translate.google.com.br/translate_a/t?client=t&text=$TEXT_TO_TRANSLATE&hl=pt-BR&sl=$SOURCE_LANGUAGE&tl=$DESTINATION_LANGUAGE&multires=1&ssel=0&tsel=0&sc=1" | iconv -f iso8859-1 -t utf-8 | awk -F'"' '{print $2}'
+curl -s -A "Mozilla/5.0" "http://translate.google.com.br/translate_a/single?client=t&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&q=$TEXT_TO_TRANSLATE&hl=pt-BR&sl=$SOURCE_LANGUAGE&tl=$DESTINATION_LANGUAGE&multires=1&ssel=0&tsel=0&sc=1" | iconv -f iso8859-1 -t utf-8 | awk -F'"' '{print $2}'
